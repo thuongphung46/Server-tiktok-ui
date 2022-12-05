22c6e10c5a84e3a692171257fcd7363e356f0363
thuongphung46 <ct040249@actvn.edu.vn>
2022-12-05 10:04:20 +0700

    chore: commit2

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const authController = require('../controllers/authControllers')
+const router = require('express').Router();
+
+
+router.post("/register", authController.registerUser);
+
+router.post("/login", authController.loginUser);
+
+
+module.exports = router;
